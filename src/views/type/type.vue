c46add70f1981b99e29fdf2bc67c96cf3d5a7c41
nn <592276614@qq.com>
2020-01-06 18:42:21 +0800

    hide

---
+++ src/views/type/type.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="type">
+      <div class="header">
+          <h2>首页</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
